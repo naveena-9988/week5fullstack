5b6e8693d3fed4ff5e0ea4c82f833b937e29cc9c
naveena-9988 <naveenaravula39.com>
2025-11-18 13:37:35 +0200

    Final: All iterations complete and synced

--- week5-be-pp1/app.js
+++ week5-be-pp1/app.js
@@ -4,10 +4,17 @@ const app = express();
 // Import the router (instead of individual functions)
 const tourRouter = require('./routes/tourRouter.js');
 
+const userRouter = require('./routes/userRouter.js');
+
 app.use(express.json());
+app.use(morgan('tiny'));
+app.use(morgan('dev'));
 
 // Use the router for all /tours routes
-app.use('/tours', tourRouter);
+app.use('/api/tours', tourRouter);
+
+app.use('/api/users', userRouter);
+
 
 const port = 4000;
 app.listen(port, () => console.log(`Server on port ${port}`));
